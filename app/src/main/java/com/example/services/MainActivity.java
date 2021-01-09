071d92517dd8fe7969b2385dfc0d7af73ed37d86
Ashishjd7 <ashish7849@gmail.com>
2021-01-09 16:10:01 +0530

    Background Services Demo-1234

---
+++ app/src/main/java/com/example/services/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.services;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+//Arpit gupta
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void start(View view)
+    {
+      //intent is use to communicate to other components
+        Intent i = new Intent(this,MyServices.class);
+        i.putExtra("name","ASHISH DHULL");
+        startService(i);
+    }
+
+    public void stop(View view)
+    {
+        Intent i = new Intent(this,MyServices.class);
+        stopService(i);
+    }
+}
